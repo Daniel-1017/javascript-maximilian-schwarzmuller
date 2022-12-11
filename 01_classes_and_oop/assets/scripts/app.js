497e1218b21d1e4fc0c782e8151e1e1b5c77af34
Daniel-17 <frimudaniel17@gmail.com>
2022-12-11 13:27:26 +0100

    Product class

--- 01_classes_and_oop/assets/scripts/app.js
+++ 01_classes_and_oop/assets/scripts/app.js
@@ -1,19 +1,31 @@
+class Product {
+  title = "DEFAULT"
+  imageUrl
+  price
+  description
+
+  constructor(title, image, desc, price) {
+    this.title = title
+    this.imageUrl = image
+    this.description = desc
+    this.price = price
+  }
+}
+
 const productsList = {
   products: [
-    {
-      title: "Pillow",
-      imageUrl:
-        "https://images.unsplash.com/photo-1584100936595-c0654b55a2e2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=737&q=80",
-      price: 19.99,
-      description: "A soft pillow.",
-    },
-    {
-      title: "Carpet",
-      imageUrl:
-        "https://images.unsplash.com/photo-1588421874990-1fe162747f9b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-      price: 89.99,
-      description: "A carpet which you might like - or not.",
-    },
+    new Product(
+      "A pillow",
+      "https://images.unsplash.com/photo-1584100936595-c0654b55a2e2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=737&q=80",
+      "A soft pillow.",
+      19.99
+    ),
+    new Product(
+      "Carpet",
+      "https://images.unsplash.com/photo-1588421874990-1fe162747f9b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      "A carpet which you might like - or not.",
+      89.99
+    ),
   ],
   render() {
     const renderHook = document.getElementById("app")
