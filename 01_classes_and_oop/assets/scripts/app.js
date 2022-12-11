4eb69ce8b4d37bfc53c5454c6b57add417153213
Daniel-17 <frimudaniel17@gmail.com>
2022-12-11 15:56:55 +0100

    super() Constructor Execution, Order & "this"

--- 01_classes_and_oop/assets/scripts/app.js
+++ 01_classes_and_oop/assets/scripts/app.js
@@ -1,9 +1,4 @@
 class Product {
-  // title = 'DEFAULT';
-  // imageUrl;
-  // description;
-  // price;
-
   constructor(title, image, desc, price) {
     this.title = title
     this.imageUrl = image
@@ -20,9 +15,11 @@ class ElementAttribute {
 }
 
 class Component {
-  constructor(renderHookId) {
+  constructor(renderHookId, shouldRender = true) {
     this.hookId = renderHookId
-    this.render()
+    if (shouldRender) {
+      this.render()
+    }
   }
 
   render() {}
@@ -82,8 +79,9 @@ class ShoppingCart extends Component {
 
 class ProductItem extends Component {
   constructor(product, renderHookId) {
-    super(renderHookId)
+    super(renderHookId, false)
     this.product = product
+    this.render()
   }
 
   addToCart() {
@@ -109,31 +107,43 @@ class ProductItem extends Component {
 }
 
 class ProductList extends Component {
-  products = [
-    new Product(
-      "A Pillow",
-      "https://www.maxpixel.net/static/photo/2x/Soft-Pillow-Green-Decoration-Deco-Snuggle-1241878.jpg",
-      "A soft pillow!",
-      19.99
-    ),
-    new Product(
-      "A Carpet",
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg",
-      "A carpet which you might like - or not.",
-      89.99
-    ),
-  ]
+  products = []
 
   constructor(renderHookId) {
     super(renderHookId)
+    this.fetchProducts()
+  }
+
+  fetchProducts() {
+    this.products = [
+      new Product(
+        "A Pillow",
+        "https://www.maxpixel.net/static/photo/2x/Soft-Pillow-Green-Decoration-Deco-Snuggle-1241878.jpg",
+        "A soft pillow!",
+        19.99
+      ),
+      new Product(
+        "A Carpet",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg",
+        "A carpet which you might like - or not.",
+        89.99
+      ),
+    ]
+    this.renderProducts()
+  }
+
+  renderProducts() {
+    for (const prod of this.products) {
+      new ProductItem(prod, "prod-list")
+    }
   }
 
   render() {
     this.createRootElement("ul", "product-list", [
       new ElementAttribute("id", "prod-list"),
     ])
-    for (const prod of this.products) {
-      new ProductItem(prod, "prod-list")
+    if (this.products && this.products.length > 0) {
+      this.renderProducts()
     }
   }
 }
