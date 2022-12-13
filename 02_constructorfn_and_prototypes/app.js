bd7ab89b0536e4b720e1b62a424cbd977a1f6886
Daniel-17 <frimudaniel17@gmail.com>
2022-12-13 19:39:33 +0100

    setting and getting prototypes

--- 02_constructorfn_and_prototypes/app.js
+++ 02_constructorfn_and_prototypes/app.js
@@ -13,5 +13,42 @@ Person.prototype = {
 }
 
 const person = new Person()
-person.greet()
-person.pringAge()
+
+const course = {
+  title: "Js The Complete Guide",
+  rating: 5,
+}
+
+Object.setPrototypeOf(course, {
+  // ...Object.getPrototypeOf(course),
+  printRating: function () {
+    console.log(`${this.rating}/5`)
+  },
+})
+
+course.printRating()
+
+const student = Object.create(
+  {
+    pringProgress: function () {
+      console.log(this.progress)
+    },
+  },
+  {
+    name: {
+      configurable: true,
+      enumerable: true,
+      value: "Daniel",
+      writable: true,
+    },
+  }
+)
+
+student.email = "test@gmail.com"
+
+Object.defineProperty(student, "progress", {
+  configurable: true,
+  enumerable: true,
+  value: 0.8,
+  writable: true,
+})
