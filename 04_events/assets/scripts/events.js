33b02e505ee054e1cf6dc345e4635c7e3794aeef
Daniel-1017 <frimudaniel17@gmail.com>
2022-12-19 16:32:31 +0200

    preventDefault

--- 04_events/assets/scripts/events.js
+++ 04_events/assets/scripts/events.js
@@ -1,8 +1,14 @@
-const buttons = document.querySelectorAll("button");
+// const buttons = document.querySelectorAll("button");
+const form = document.querySelector("form");
 
 const buttonClickHandler = (event) => {
   event.target.disabled = true;
   console.log("Hello World!", event);
 };
 
-buttons.forEach((btn) => btn.addEventListener("click", buttonClickHandler));
+// buttons.forEach((btn) => btn.addEventListener("click", buttonClickHandler));
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log("Submited");
+});
